3281d386b7df66db0039342dc66f2e30e66b589a
pat-nel87 <71235856+pat-nel87@users.noreply.github.com>
2021-02-26 22:36:16 -0500

    Working version

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -7,47 +7,63 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <windows.h> //included to modify default terminal background color!
+
 
 using namespace std;
 
-void pollResult(string names[], int vtot[]);
+void pollResult(string names[], double vtot[]);
 
 int main() {
-		
+	system("Color 0A"); // Makes background black & text monochrome green!	
+	
 	string last_name[5];
-	int votes[5];
+	double votes[5];
+	double max = votes[0];
+	string winner;
 	
-	
-	for (int i=0; i < 5; i++) {
+	for (int i=0; i < 5; i++)
+	{
 		cout << "Enter Candidate " << i + 1 << "'s last name " << " ";
 		getline(cin, last_name[i]);				
 	}
-	for (int j=0; j < 5; j++) {
+	for (int j=0; j < 5; j++)
+	{
 		cout << "Enter Candidate " << j + 1 << "'s total votes" << " ";
 		cin >> votes[j];
+		if (max < votes[j]) //finds the winner
+		{
+			max = votes[j];
+			winner = last_name[j];
+		}
 	}
-	
+		
 	pollResult(last_name, votes);
+	cout << "\n" << "The Winner of the election is: " << winner << endl;
 	
 	return 0;
 }
 
-void pollResult(string names[], int vtot[]) {
+void pollResult(string names[], double vtot[])
+{
 	
-	int total;
-	float percent;
+	double total;
+	double percent;
+	for(int h=0; h < 5; h++)
+{
+	total = total + vtot[h];
+}	
 	
-	cout << "CANDIDATE		" << "Votes Received		" << "Percent of Total Votes" << "\n";
+	cout << "CANDIDATE	" << "Votes Received	" << "Percent of Total Votes" << "\n";
 	for (int k=0; k < 5; k++)
 {
-	cout << names[k] << "		" << vtot[k] << "		" << " \n";
-
-
+	percent = (vtot[k] / total);
+	percent = percent * 100;
+	cout << names[k] << "		" << vtot[k] << "		" << percent << "%" << " \n";
 
 }
 
-
-
+ cout << "\n" << "Total Votes: " << total << endl;
 
 }
 
