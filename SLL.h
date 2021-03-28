a951ce42a954c5c8e8a64453dafe1b840ff802ab
pat-nel87 <71235856+pat-nel87@users.noreply.github.com>
2021-03-28 12:47:26 -0400

    fooling around with implementation of a singly linked list

---
+++ SLL.h
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class node
+{
+	private:
+		int nxtNodeId;
+		int Id;
+	
+	public:
+		void setNext(int);
+		int getId();
+		int getNext();
+		node() {
+			cout << "node Created!" << endl;
+			cout << "Enter the node ID#: ";
+			cin >> Id;
+		}
+};
+
+
+class headNode
+{
+	private:
+		int nextNodeId;
+	
+	public:
+		void setNextNode(int);
+		headNode()
+		{
+			cout << "headNode created!";			
+		}
+		
+};
+
+int node::getId()
+{
+	
+	return Id;
+}
+
+void headNode::setNextNode(int next)
+{
+	int booler;
+	nextNodeId = next;
+	cout << " next node is set to ID: " << nextNodeId << endl;
+	cout << " Would you like to create another node(enter 0 or 1): ";
+	cin >> booler;
+	if (booler == 1)
+	{
+		new node;	
+			
+	} 
+	
+}
+
+void node::setNext(int nxt)
+{
+	nxtNodeId = nxt;
+}
+
+int node::getNext()
+{
+	return nxtNodeId;
+}
+		
