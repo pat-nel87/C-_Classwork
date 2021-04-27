ccde77d1ef6698eb648d71990bb3de3760a2bdd0
Patrick Nelson <patnelson2022@gmail.com>
2021-04-27 17:32:21 -0400

    organized into folder

---
+++ Assignments/Assignment6.cpp
@@ -0,0 +1,35 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int count(int num);
+int sumSquares(int num, int sum);
+
+int main() {
+    int num = 100;
+    int sum = 0;
+    sum = sumSquares(100, 0);
+
+    cout << "Total Sum is " << sum << endl;
+
+    return 0;
+}
+
+int sumSquares(int num, int sum)
+{
+    if (num == 0)
+    {
+        return sum;
+    }
+
+        cout << "Pre-Calculation Sum is: " << sum << endl;
+        cout << "Pre-Decrement Num is: " << num << endl;
+        sum = (num * num) + sum;
+        num--;
+        cout << " Num is now: " << num << endl;
+        sumSquares(num,  sum);
+
+}
