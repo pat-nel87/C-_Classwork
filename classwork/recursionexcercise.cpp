e2b339821eb8f15a955453e214de32d4140fb85c
Patrick Nelson <patnelson2022@gmail.com>
2021-04-27 21:06:42 -0400

    Recursion excercise

---
+++ classwork/recursionexcercise.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int starRec(int num, int storage);
+int invert(int num2, int start);
+
+
+int main()
+{
+    int prompt;
+    cout << "Please enter the number to generate pattern: ";
+    cin >> prompt;
+    starRec(prompt, prompt);
+    return 0;
+}
+
+int starRec(int num, int storage)
+{
+    if (num == 0)
+    {
+        return invert(num, storage);
+    }
+    int temp = num;
+    while(temp != 0){
+        cout << "*";
+        temp--;
+    }
+    cout << endl;
+    num--;
+    return starRec(num, storage);
+
+}
+
+int invert(int num2, int start)
+{
+   // cout << num2 << "" << start << endl;
+    if (num2 == start){
+        return num2;
+    }
+
+    int temp2 = num2 + 1;
+    while (temp2 != 0)
+    {
+        cout << "*";
+        temp2--;
+    }
+    num2++;
+    cout << endl;
+    return invert(num2, start);
+}
